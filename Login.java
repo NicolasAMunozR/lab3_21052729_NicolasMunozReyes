067185c00648991539cb4abb64edb85c93a0d158
nicoxd104 <nicolas.munoz.r@usach.cl>
2023-06-22 14:04:24 -0400

    Login.java
    Login creado

---
+++ Login.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Login {
+    String nombre;
+    public Login(String user_login) {
+       this.nombre = user_login;
+    }
+    @Override
+    public String toString() {
+        return "Ususario logeado = {" +
+                nombre + '}';
+    }
+
+    public String getnombre() {
+        return nombre;
+    }
+}
