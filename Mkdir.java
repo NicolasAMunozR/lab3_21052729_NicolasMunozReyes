4d2ffed2b655b502225a522403a7182e4a4cf80a
nicoxd104 <nicolas.munoz.r@usach.cl>
2023-07-08 02:25:06 -0400

    Mkdir.java
    Modificación del Mkdir

--- Mkdir.java
+++ Mkdir.java
@@ -1,24 +1,54 @@
 package org.example;
 
 import java.util.Date;
-
 public class Mkdir {
-    String nombre;
-    Date fechaCreacion;
-    String usuario_log;
-    Switchdrive ruta;
-    public Mkdir(String nombre, String usuario_log, Switchdrive ruta) {
-        this.nombre = nombre;
-        this.fechaCreacion = new Date();
-        this.usuario_log = usuario_log;
-        this.ruta = ruta;
+    private Date fechaCreacion;
+    private Date fechamodificacion;
+    private final String usuario_log;
+    private String direccion;
+    private String prueba;
+    public String getDireccion() {
+        return direccion;
     }
+    public void setFechaCreacion(Date fechaCreacion) {
+        this.fechaCreacion = fechaCreacion;
+    }
+
+    public Date getFechaCreacion() {
+        return fechaCreacion;
+    }
+
+    public Mkdir(String direccion, String usuario_log) {
+        this.direccion = direccion;
+        this.fechaCreacion = new Date();
+        this.fechamodificacion = new Date();
+        this.usuario_log = usuario_log;
+    }
+
+    public String getPrueba() {
+        return prueba;
+    }
+
+    public String getUsuario_log() {
+        return usuario_log;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public void setFechamodificacion(Date fechamodificacion) {
+        this.fechamodificacion = fechamodificacion;
+    }
+
     @Override
     public String toString() {
-        return "Carpeta = {" +
-                ruta + nombre + "/" +
-                ", Fecha = " + fechaCreacion +
+        prueba = "Carpeta = {" +
+                direccion +
+                ", Fecha de creación = " + fechaCreacion +
+                ", Fecha de ultima modificación = " + fechamodificacion +
                 ", Usuario = " + usuario_log +
-                '}';
+                "}\n";
+        return prueba;
     }
 }
