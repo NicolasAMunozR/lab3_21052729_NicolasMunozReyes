527f984de08ec351ac5c638d4752b9c9477891d8
nicoxd104 <nicolas.munoz.r@usach.cl>
2023-07-08 02:23:00 -0400

    Documento.java
    Creación del tipo de archivo Documento

---
+++ Documento.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class Documento extends File{
+    public Documento(String nombre, String extension, String contenido) {
+        super(nombre, extension, contenido);
+    }
+}
