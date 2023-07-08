8f8c36fc7c88f41ee594560387f0bd9a96026987
nicoxd104 <nicolas.munoz.r@usach.cl>
2023-07-08 02:23:17 -0400

    Texto.java
    Creación del tipo de archivo Texto

---
+++ Texto.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class Texto extends File {
+    public Texto(String nombre, String extension, String contenido) {
+        super(nombre, extension, contenido);
+    }
+}
