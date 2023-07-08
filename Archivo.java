bcc72e5d06f34013e145abf21222504871cddbf1
nicoxd104 <nicolas.munoz.r@usach.cl>
2023-07-08 02:22:42 -0400

    Archivo.java
    Creación del tipo de archivo Archivo

---
+++ Archivo.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class Archivo extends File {
+    public Archivo(String nombre, String extension, String contenido) {
+        super(nombre, extension, contenido);
+    }
+}
